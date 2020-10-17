34dcd8416936dff29d369203a5cf7f162a64e528
ShoSeshake <sho122seshake@gmail.com>
2020-10-17 10:46:31 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -3,46 +3,71 @@ import "./App.css";
 import PropTypes from 'prop-types'
 
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div className="header">
+//           <h1>Hello World</h1>
+//         </div>
+//         <div>{
+//           profiles.map((profile, index) => {
+//             return <User name={profile.name} age={profile.age} key={index}/>
+//           })
+//           }
+
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (<Counter></Counter>);
+};
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count:0}
+  };
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
   render() {
     return (
-      <React.Fragment>
-        <div className="header">
-          <h1>Hello World</h1>
-        </div>
-        <div>{
-          profiles.map((profile, index) => {
-            return <User name={profile.name} age={profile.age} key={index}/>
-          })
-          }
-
-        </div>
-      </React.Fragment>
+      <div>
+        Counter
+          <div>{this.state.count}</div>
+        <button name="+" onClick={() =>{ this.handlePlusButton()}}>+</button>
+        <button name="-" onClick={() =>{ this.handleMinusButton()}}>-</button>
+      </div>
     );
   }
 }
 
-// const App = () => {
-//   return (<div>
-//     <User name={"taro"} age={14}/>
-//     </div>);
+
+// const profiles= [
+//   {name:"taro",age:10},
+//   {name:"hanako",age:18},
+
+// ]
+
+// const User = (props) => {
+//   return(
+//     <div>{props.name}<br/>{props.age}</div>
+//   );
 // };
-const profiles= [
-  {name:"taro",age:10},
-  {name:"hanako",age:18},
 
-]
-
-const User = (props) => {
-  return(
-    <div>{props.name}<br/>{props.age}</div>
-  );
-};
-
-User.PropTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number
-}
+// User.PropTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number
+// }
 
 
 export default App;
