fb503a7f01765736e660b72af30d69d2b3b97f37
ShoSeshake <sho122seshake@gmail.com>
2020-10-17 10:02:08 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import PropTypes from 'prop-types'
 
-class App extends Component{
+
+class App extends Component {
   render() {
-    return(
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    )
+    return (
+      <React.Fragment>
+        <div className="header">
+          <h1>Hello World</h1>
+        </div>
+        <div>{
+          profiles.map((profile, index) => {
+            return <User name={profile.name} age={profile.age} key={index}/>
+          })
+          }
+
+        </div>
+      </React.Fragment>
+    );
   }
 }
+
+// const App = () => {
+//   return (<div>
+//     <User name={"taro"} age={14}/>
+//     </div>);
+// };
+const profiles= [
+  {name:"taro",age:10},
+  {name:"hanako",age:18},
+
+]
+
+const User = (props) => {
+  return(
+    <div>{props.name}<br/>{props.age}</div>
+  );
+};
+
+User.PropTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number
+}
+
 
 export default App;
